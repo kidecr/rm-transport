54b3ce0801b5e93c210fb2911efc65c002465b14
kidecr <2290483572@qq.com>
2024-11-17 14:18:59 +0800

    完善了根据包id指定接收并输出，用于debug

--- test/RecvPackage.cpp
+++ test/RecvPackage.cpp
@@ -11,51 +11,57 @@
 
 struct ArgsParser
 {
-    PORT_TYPE device_type;
+    PORT_TYPE device_type = PORT_TYPE::NONE;
     std::string port_name;
-    int32_t baud;
+    int32_t baud = 0;
+    bool print_buffer = false;
     std::vector<uint32_t> id;
 };
 
 ArgsParser argsParser(int argc, char* argv[])
 {
     ArgsParser args_parser;
-    if( argc <= 5 
-        || std::string(argv[1]) != "--device-type"
-        || std::string(argv[3]) != "--port-name")
-    {
-        std::cout << "输入参数错误" << std::endl;
-        std::cout << "command: RecvPackage --device-type <serial|can> --port-name <port name> [--baud <baud>] [--id <id,...>]" << std::endl;
-        args_parser.device_type = PORT_TYPE::NONE;
-        return args_parser;
+    // 首先判定输入是否合理
+    enum class ARGS_TYPE {
+        NORMAL, EMPTY, ILLEGAL, HELP
+    };
+    ARGS_TYPE args_type = ARGS_TYPE::NORMAL;
+    if (argc == 1) {
+        args_type = ARGS_TYPE::EMPTY;
     }
-    // 获取设备类型
-    std::string device_type(argv[2]);
-    if(device_type == "serial"){
-        args_parser.device_type = PORT_TYPE::SERIAL;
-        if (argc >= 7 && std::string(argv[5]) == "--baud")
-        {
-            args_parser.baud = std::stoi(argv[6]);
+    else
+    {
+        if (std::string(argv[1]) == "--help" || std::string(argv[1]) == "-h"){
+            args_type = ARGS_TYPE::HELP;
         }
-        else if (argc >= 9 && std::string(argv[7]) == "--baud")
-        {
-            args_parser.baud = std::stoi(argv[8]);
+        else if (std::string(argv[1]) == "--device-type" || std::string(argv[1]) == "-d"){
+            if (argc >= 4) {
+                if (std::string(argv[3]) == "--port-name" || std::string(argv[3]) == "-p") {
+                    args_type = ARGS_TYPE::NORMAL;
+                }
+                else {
+                    args_type = ARGS_TYPE::ILLEGAL;
+                }
+            }
+            else{
+                args_type = ARGS_TYPE::ILLEGAL;
+            }
         }
         else{
-            args_parser.device_type = PORT_TYPE::NONE;
-            std::cout << "无波特率输入" << std::endl;
-            return args_parser;
+            args_type = ARGS_TYPE::ILLEGAL;
         }
     }
-    else if (device_type == "can")
-        args_parser.device_type = PORT_TYPE::CAN;
-    else {
+    // 当存在非法输入或help时，处理输出
+    if (args_type != ARGS_TYPE::NORMAL) {
+        if (args_type == ARGS_TYPE::EMPTY)
+            std::cout << "输入参数不可以为空" << std::endl;
+        else if (args_type == ARGS_TYPE::ILLEGAL)
+            std::cout << "输入参数错误" << std::endl;
+        std::cout << "command: RecvPackage [--help|-h] --device-type|-d <serial|can> --port-name|-p <port name> [--baud|-b <baud>] [--id|-i <id,...>] [--print-buffer|-pb]" << std::endl;
         args_parser.device_type = PORT_TYPE::NONE;
-        std::cout << "无输入类型设备" << std::endl;
         return args_parser;
     }
-    // 获取设备名
-    args_parser.port_name = std::string(argv[4]);
+
     // 获取id列表
     auto getIdList = [](std::string id_list_str) -> std::vector<uint32_t> {
         std::regex validIdListPattern("0x[0-9A-Fa-f]{3}(,0x[0-9A-Fa-f]{3})*");
@@ -75,20 +81,74 @@ ArgsParser argsParser(int argc, char* argv[])
         }
         return id_list;
     };
-    // 有id列表
-    if (argc >= 7 && std::string(argv[5]) == "--id")
-    {
-        args_parser.id = getIdList(std::string(argv[6]));
-    }
-    if (argc >= 9 && std::string(argv[7]) == "--id"){
-        args_parser.id = getIdList(std::string(argv[8]));
+    // 解析参数
+    for (int i = 1; i < argc; ++i) {
+        std::string current_argv = argv[i];
+        if (current_argv == "--device-type" || current_argv == "-d") {
+            if (i + 1 < argc && argv[i+1][0] != '-') {
+                std::string device_type = argv[i + 1];
+                if(device_type == "serial"){
+                    args_parser.device_type = PORT_TYPE::SERIAL;
+                }
+                else if (device_type == "can")
+                    args_parser.device_type = PORT_TYPE::CAN;
+                else {
+                    args_parser.device_type = PORT_TYPE::NONE;
+                    std::cout << "非可识别输入类型设备" << std::endl;
+                    return args_parser;
+                }
+            }
+            else {
+                std::cout << "cannot parser argument --device-type|-d" << std::endl;
+                args_parser.device_type = PORT_TYPE::NONE;
+                return args_parser;
+            }
+        }
+        else if (current_argv == "--port-name" || current_argv == "-p") {
+            if (i + 1 < argc && argv[i+1][0] != '-') {
+                args_parser.port_name = argv[i + 1];
+            }
+            else {
+                std::cout << "cannot parser argument --port-name|-p" << std::endl;
+                args_parser.device_type = PORT_TYPE::NONE;
+                return args_parser;
+            }
+        }
+        else if (current_argv == "--baud" || current_argv == "-b") {
+            if (i + 1 < argc && argv[i+1][0] != '-') // 后续存在参数且不为--或-
+                args_parser.baud = std::stoi(argv[i + 1]);
+            else{
+                std::cout << "cannot parser argument --baud|b" << std::endl;
+                args_parser.device_type = PORT_TYPE::NONE;
+                return args_parser;
+            }
+        }
+        else if (current_argv == "--id" || current_argv == "-i") {
+            if (i + 1 < argc && argv[i + 1][0] != '-')
+                args_parser.id = getIdList(std::string(argv[i + 1]));
+            else {
+                std::cout << "cannot parser argument --id|-i" << std::endl;
+                args_parser.device_type = PORT_TYPE::NONE;
+                return args_parser;
+            }
+        }
+        else if (current_argv == "--print-buffer" || current_argv == "-pb") {
+            args_parser.print_buffer = true;
+        }
     }
     
     return args_parser;
 }
 
+class BasePackagePort
+{
+public:
+    virtual std::string printIdList() = 0;
+    virtual void registerIDList(std::vector<uint32_t> &id_list) = 0;
+};
+
 template<typename T>
-class PackageReceiverPort : public T
+class PackageReceiverPort : public T, public BasePackagePort
 {
 public:
     std::vector<ID> m_recv_ids;
@@ -106,6 +166,8 @@ public:
             ss << "0x" << std::hex << transport::unmask(id) << "\n";
         return ss.str();
     }
+
+    void registerIDList(std::vector<uint32_t> &id_list) {}
 protected:
     std::shared_mutex m_id_vec_mutex;
 
@@ -128,26 +190,89 @@ protected:
     }
 };
 
+template<typename T>
+class PackagePrinterPort : public T, public BasePackagePort
+{
+public:
+    std::vector<ID> m_id_list;
+    // for serial
+    PackagePrinterPort(std::string port_name, int baud_read) : T(port_name, baud_read) {}
+    // for can
+    PackagePrinterPort(std::string port_name) : T(port_name) {}
+
+    void registerIDList(std::vector<uint32_t> &id_list) {
+        std::lock_guard<std::shared_mutex> lock(m_id_vec_mutex);
+        for (auto id : id_list){
+            m_id_list.push_back(id);
+        }
+    }
+
+    std::string printIdList()
+    {
+        std::stringstream ss;
+        std::lock_guard<std::mutex> lock(m_msg_vec_mutex);
+        ss << "------------------\n";
+        for(auto &msg : m_msg_list) {
+            auto [id, buffer] = msg;
+            ss << "0x" << std::hex << transport::unmask(id) << ": " << buffer << "\n";
+        }
+        m_msg_list.clear();
+        return ss.str();
+    }
+protected:
+    std::vector<std::tuple<ID, transport::Buffer>> m_msg_list;
+    std::mutex m_msg_vec_mutex;
+    std::shared_mutex m_id_vec_mutex;
+
+    bool findID(ID& id){
+        std::shared_lock<std::shared_mutex> lock(m_id_vec_mutex);
+        for (auto current_id : m_id_list)
+        {
+            if (transport::unmask(id) == current_id)
+                return true;
+        }
+        return false;
+    }
+    virtual bool recvOnePackage(ID &id, transport::Buffer &buffer) {
+        
+        if(findID(id)){
+            auto tuple = std::make_tuple(id, buffer);
+            std::lock_guard<std::mutex> lock(m_msg_vec_mutex);
+            m_msg_list.push_back(tuple);
+            return true;
+        }
+        return false;
+    }
+};
+
 int main(int argc, char* argv[]){
     ArgsParser args_parser = argsParser(argc, argv);
     if(args_parser.device_type == PORT_TYPE::NONE){
         return -1;
     }
     LOGINIT("RecvPackage");
-    if(args_parser.device_type == PORT_TYPE::SERIAL){
-        PackageReceiverPort<transport::SerialPort> serial_port(args_parser.port_name, args_parser.baud);
-        while (transport::ok())
-        {
-            std::cout << serial_port.printIdList() << std::endl;
-            usleep(1e6); // sleep 1s
+    std::shared_ptr<BasePackagePort> port = nullptr;
+    if (args_parser.print_buffer) {
+        if(args_parser.device_type == PORT_TYPE::SERIAL){
+            port = std::make_shared<PackagePrinterPort<transport::SerialPort>>(args_parser.port_name, args_parser.baud);
+        }
+        else if (args_parser.device_type == PORT_TYPE::CAN){
+            port = std::make_shared<PackagePrinterPort<transport::CanPort>>(args_parser.port_name);
+        }
+        port->registerIDList(args_parser.id);
+    }
+    else {
+        if(args_parser.device_type == PORT_TYPE::SERIAL){
+            port = std::make_shared<PackageReceiverPort<transport::SerialPort>>(args_parser.port_name, args_parser.baud);
+        }
+        else if (args_parser.device_type == PORT_TYPE::CAN){
+            port = std::make_shared<PackageReceiverPort<transport::CanPort>>(args_parser.port_name);
         }
     }
-    else if (args_parser.device_type == PORT_TYPE::CAN){
-        PackageReceiverPort<transport::CanPort> can_port(args_parser.port_name);
-        while (transport::ok())
-        {
-            std::cout << can_port.printIdList() << std::endl;
-            usleep(1e6); // sleep 1s
-        }
+    while (transport::ok())
+    {
+        std::cout << port->printIdList() << std::endl;
+        usleep(1e6);
     }
+    
 }
